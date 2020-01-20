ca27fe9668c94ad491947ec7126ee84556bec6a7
guruduttperi <thegdaproject@gmail.com>
2020-01-19 19:46:30 -0500

    Initial Commit

---
+++ precog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  precog
+//
+//  Created by Gurudutt on 1/19/20.
+//  Copyright © 2020 Gurudutt Perichetla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
